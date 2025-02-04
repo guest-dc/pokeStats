76566122aa09cac23dec34a6eefc14cfcf0c8c46
Davis Guest <davisdevice@gmail.com>
2025-02-04 00:20:09 -0500

    added config.js file, removed old firebase files, WIP menu button positioning

---
+++ js/config.js
@@ -0,0 +1,7 @@
+
+// Global Variables
+const DATABASE_URL = "database.html";
+const DMG_SPREADSHEET_URL = "dmg-spreadsheet.html";
+const DOCUMENTATION_URL = "documentation.html";
+const RESOURCES_URL = "resources.html";
+
