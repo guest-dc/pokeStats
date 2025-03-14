19ccb9e7add82c76e9dde57eb4b73de52b9fd9b1
Davis Guest <davisdevice@gmail.com>
2025-03-13 21:23:30 -0400

    added  jquery dataTable hosting

--- data/genDatabase.py
+++ data/genDatabase.py
@@ -1,11 +1,31 @@
 import requests
 import json
+import csv
 
 FILENAME = "./data/database.json"
+CSV_FILE = "./data/releasedMons.csv"
 API_URL = "https://pokemon-go-api.github.io/pokemon-go-api/api/pokedex.json"
 
-response = requests.get(API_URL)
 
+
+# Function to read the CSV and return a dictionary mapping id to (isReleased, hasShadow)
+def load_csv_data(csv_file):
+    release_data = {}
+    with open(csv_file, mode='r', newline='', encoding='utf-8') as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            release_data[row['pokeID']] = {
+                'isReleased': row['isReleased'].strip() == 'True',
+                'hasShadow': row['hasShadow'].strip() == 'True',
+                'imgID':row['dexID']
+            }
+    return release_data
+
+
+print("Processing API data...")
+
+
+response = requests.get(API_URL)
 if response.status_code == 200:
     data = response.json()
     
@@ -13,5 +33,31 @@ if response.status_code == 200:
         json.dump(data, file, indent=4)
     
     print(f"JSON data saved successfully as '{FILENAME}'")
+
 else:
     print(f"Failed to fetch data. Status Code: {response.status_code}")
+
+
+print("Adding availability dexID to JSON...")
+
+
+release_data = load_csv_data(CSV_FILE)
+
+with open(FILENAME, 'r', encoding='utf-8') as f:
+    pokemon_data = json.load(f)
+
+for pokemon in pokemon_data:
+    poke_id = pokemon['id']
+
+    if poke_id in release_data:
+        pokemon.update(release_data[poke_id])
+    
+    if pokemon.get('regionForms'):
+        for region_form in pokemon['regionForms']:
+            if region_form in release_data:
+                pokemon['regionForms'][region_form].update(release_data[region_form])
+
+with open(FILENAME, 'w', encoding='utf-8') as f:
+    json.dump(pokemon_data, f, indent=4)
+
+print(f"JSON data successfully updated")
--- js/database_scripts.js
+++ js/database_scripts.js
@@ -1,4 +1,7 @@
 
+const COL_SPAN = 5;
+
+
 skippedVarients = [
      "Pikachu", "Eevee",
      "Espeon", "Umbreon", "Entei", "Raikou", "Suicune", "Lugia", "Ho-Oh",
@@ -9,7 +12,8 @@ skippedVarients = [
      "Pumpkaboo", "Gourgeist",
      "Rockruff", "Minior", "Mimikyu", "Magearna",
      "Sinistea", "Polteageist", "Morpeko", "Eternatus",
-     "Oikologne", "Maushold", "Squawkabilly", "Tatsugiri", "Dudunsparce", "Koraidon", "Miraidon"]
+     "Oikologne", "Maushold", "Squawkabilly", "Tatsugiri", "Dudunsparce", "Koraidon", "Miraidon"
+]
 
 function getPokemonIcon(id, gen) {
      return `<img src="/images/sprites/gen${gen}/${id}_icon.png" class="pokemon-icon" alt="${id}">`;
@@ -23,74 +27,93 @@ function getMegaIcon() {
      return `<img src="/images/icons/mega_icon.png" class="mega-icon" alt="mega">`;
 }
 
-fetch('/data/database.json')
-     .then(response => response.json())
-     .then(data => {
-          let tableBody = document.getElementById('pokemon-table-body');
-          tableBody.innerHTML = '';
+function getShadowIcon() {
+     return `<img src="/images/icons/shadow_icon.png" class="shadow-icon" alt="shadow">`;
+}
 
-          let filteredData = data.filter(pokemon => pokemon.assetForms && pokemon.stats && pokemon.assetForms.length > 0);
+function createRow(pokemon) {
+     let dexNr = pokemon.dexNr;
+     let name = pokemon.names.English;
+     
+     let primaryType = pokemon.primaryType.names.English;
+     let secondaryType = pokemon.secondaryType ? pokemon.secondaryType.names.English : "";
+     let typeIcons = getTypeIcon(primaryType);
+     if (secondaryType) typeIcons += getTypeIcon(secondaryType);
 
-          filteredData.forEach(pokemon => {
-               // format pokemon cell
-               let icon = "";
-               let poke_name = pokemon.names.English;
-               
-               // format typing cell
-               let primaryType = pokemon.primaryType.names.English;
-               let secondaryType = pokemon.secondaryType ? pokemon.secondaryType.names.English : "";
+     let megaIcon = pokemon.hasMegaEvolution ? getMegaIcon() : "";
+     let shadowIcon = pokemon.hasShadow ? getShadowIcon() : "";
 
-               let typeIcons = getTypeIcon(primaryType);
-               if (secondaryType) typeIcons += getTypeIcon(secondaryType);
+     let row = document.createElement("tr");
+     row.innerHTML = `
+          <td>${dexNr}</td>
+          <td>${name}</td>
+          <td id="type-icons">${typeIcons}</td>
+          <td id="mega-icon">${megaIcon}</td>
+          <td id="shadow-icon">${shadowIcon}</td>
+     `;
+     return row;
+}
 
-               let megaIcon = pokemon.hasMegaEvolution ? getMegaIcon() : "";
+function fetchDataAndRender() {
+     return new Promise((resolve, reject) => {
+          fetch('/data/database.json')
+          .then(response => response.json())
+          .then(data => {
+               const tableBody = document.querySelector('#databaseTable tbody');
+               tableBody.innerHTML = '';     // Clear existing rows
 
-               let row = `
-                    <tr>
-                         <td>${pokemon.dexNr}</td>
-                         <td class="pokemon-container">${poke_name}</td>
-                         <td class="type-icons">${typeIcons}</td>
-                         <td>${megaIcon}</td>
-                         <td></td>
-                    </tr>
-               `;
-               tableBody.innerHTML += row;
+               data = data.filter(pokemon => pokemon.isReleased);
 
-               // insert regionals
-               if (pokemon.regionForms && !skippedVarients.includes(poke_name)) {
-                    Object.keys(pokemon.regionForms).forEach(regionKey => {
-                         let regional = pokemon.regionForms[regionKey];
+               for (const pokeData in data) {
+                    
+                    // insert pokemon
+                    const pokemon = data[pokeData];
+                    const row = createRow(pokemon);
+                    tableBody.appendChild(row);
 
-                         let poke_name = regional.names.English;
-                         
-                         // format typing cell
-                         let primaryType = regional.primaryType.names.English;
-                         let secondaryType = regional.secondaryType ? regional.secondaryType.names.English : "";
-
-                         let typeIcons = getTypeIcon(primaryType);
-                         if (secondaryType) typeIcons += getTypeIcon(secondaryType);
-
-                         let megaIcon = regional.hasMegaEvolution ? getMegaIcon() : "";
-
-                         let row = `
-                              <tr>
-                                   <td>${regional.dexNr}</td>
-                                   <td class="pokemon-container">${poke_name}</td>
-                                   <td class="type-icons">${typeIcons}</td>
-                                   <td>${megaIcon}</td>
-                                   <td></td>
-                              </tr>
-                         `;
-                         tableBody.innerHTML += row;
-                    });
+                    // insert regionals and varients
+                    if (pokemon.regionForms && !skippedVarients.includes(pokemon.names.English)) {
+                         Object.keys(pokemon.regionForms).forEach(regionKey => {
+                              const regional = pokemon.regionForms[regionKey];
+                              const row = createRow(regional);
+                              tableBody.appendChild(row);
+                         });
+                    }
                }
-          });
 
-          if (filteredData.length === 0) {
-               tableBody.innerHTML = '<tr><td colspan="3">No Pokémon found.</td></tr>';
-          }
-     })
-     .catch(error => {
-          console.error('Error fetching Pokémon:', error);
-          document.getElementById('pokemon-table-body').innerHTML = '<tr><td colspan="3">Failed to load data</td></tr>';
+               if (data.length === 0) {
+                    tableBody.innerHTML = `<tr><td colspan="${COL_SPAN}">No Pokémon found.</td></tr>`;
+               }
+               resolve();
+          })
+          .catch(error => {
+               console.error('Error fetching Pokémon: ', error);
+               reject(error);
+          });
      });
+}
+
+
+
+$(document).ready(function() {
+
+     function renderTable() {
+          fetchDataAndRender().then(function() {
+               var table = $('#databaseTable').DataTable();
+         
+               table.clear().draw();
+         
+               // Re-populate DataTable with updated data
+               fetchDataAndRender().then(function() {
+                 table.rows.add($('#databaseTable tbody tr')).draw();
+               });
+         
+          }).catch(function(error) {
+               console.error('Error rendering table:', error);
+          });
+     }
+
+     // Initial rendering
+     renderTable();
+
+});
